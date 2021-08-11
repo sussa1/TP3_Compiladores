3079b474a4383cd3651e88398d0976c9476e5853
Vitor Rodarte <vitorricoy@gmail.com>
2021-08-11 15:28:41 -0300

    Script para executar os testes

--- TP3/testes/run_tests.sh
+++ TP3/testes/run_tests.sh
@@ -1,13 +1,130 @@
-EXEC=$1
-TMP_OUT=$2
+TMP_OUT1=test1.results
+TMP_OUT2=test2.results
 
-for i in {0..10..1}; do
+echo "Testes Case List"
+
+for i in {1..4..1}; do
   testname=$(printf "%d" $i)
-  python3 $EXEC < tests/$testname.in > $TMP_OUT
-  if ! diff -qwB tests/$testname.out $TMP_OUT &>/dev/null; then
-    echo "Test $testname failed"
+  ./myparser testes/case_list/t$testname.cl > $TMP1_OUT
+  ./myparserCorrecto testes/case_list/t$testname.cl > $TMP2_OUT
+
+  if ! diff -qwB $TMP_OUT1 $TMP_OUT2 &>/dev/null; then
+    echo "Teste $testname falhou"
   else
-    echo "Test $testname passed"
+    echo "Teste $testname passou"
   fi
 done
-rm $TMP_OUT
+rm $TMP_OUT1
+rm $TMP_OUT2
+
+echo "Testes Class List"
+
+for i in {1..3..1}; do
+  testname=$(printf "%d" $i)
+  ./myparser testes/class_list/t$testname.cl > $TMP1_OUT
+  ./myparserCorrecto testes/class_list/t$testname.cl > $TMP2_OUT
+
+  if ! diff -qwB $TMP_OUT1 $TMP_OUT2 &>/dev/null; then
+    echo "Teste $testname falhou"
+  else
+    echo "Teste $testname passou"
+  fi
+done
+rm $TMP_OUT1
+rm $TMP_OUT2
+
+echo "Testes Expression"
+
+for i in {1..2..1}; do
+  testname=$(printf "%d" $i)
+  ./myparser testes/expression/t$testname.cl > $TMP1_OUT
+  ./myparserCorrecto testes/expression/t$testname.cl > $TMP2_OUT
+
+  if ! diff -qwB $TMP_OUT1 $TMP_OUT2 &>/dev/null; then
+    echo "Teste $testname falhou"
+  else
+    echo "Teste $testname passou"
+  fi
+done
+rm $TMP_OUT1
+rm $TMP_OUT2
+
+echo "Testes Expression Let"
+
+for i in {1..6..1}; do
+  testname=$(printf "%d" $i)
+  ./myparser testes/expression_let/t$testname.cl > $TMP1_OUT
+  ./myparserCorrecto testes/expression_let/t$testname.cl > $TMP2_OUT
+
+  if ! diff -qwB $TMP_OUT1 $TMP_OUT2 &>/dev/null; then
+    echo "Teste $testname falhou"
+  else
+    echo "Teste $testname passou"
+  fi
+done
+rm $TMP_OUT1
+rm $TMP_OUT2
+
+echo "Testes Expression List Comma"
+
+for i in {1..4..1}; do
+  testname=$(printf "%d" $i)
+  ./myparser testes/expression_list_comma/t$testname.cl > $TMP1_OUT
+  ./myparserCorrecto testes/expression_list_comma/t$testname.cl > $TMP2_OUT
+
+  if ! diff -qwB $TMP_OUT1 $TMP_OUT2 &>/dev/null; then
+    echo "Teste $testname falhou"
+  else
+    echo "Teste $testname passou"
+  fi
+done
+rm $TMP_OUT1
+rm $TMP_OUT2
+
+echo "Testes Expression List Semicolon"
+
+for i in {1..3..1}; do
+  testname=$(printf "%d" $i)
+  ./myparser testes/expression_list_semic/t$testname.cl > $TMP1_OUT
+  ./myparserCorrecto testes/expression_list_semic/t$testname.cl > $TMP2_OUT
+
+  if ! diff -qwB $TMP_OUT1 $TMP_OUT2 &>/dev/null; then
+    echo "Teste $testname falhou"
+  else
+    echo "Teste $testname passou"
+  fi
+done
+rm $TMP_OUT1
+rm $TMP_OUT2
+
+echo "Testes Feature List"
+
+for i in {1..4..1}; do
+  testname=$(printf "%d" $i)
+  ./myparser testes/feature_list/t$testname.cl > $TMP1_OUT
+  ./myparserCorrecto testes/feature_list/t$testname.cl > $TMP2_OUT
+
+  if ! diff -qwB $TMP_OUT1 $TMP_OUT2 &>/dev/null; then
+    echo "Teste $testname falhou"
+  else
+    echo "Teste $testname passou"
+  fi
+done
+rm $TMP_OUT1
+rm $TMP_OUT2
+
+echo "Testes Formal List"
+
+for i in {1..4..1}; do
+  testname=$(printf "%d" $i)
+  ./myparser testes/formal_list/t$testname.cl > $TMP1_OUT
+  ./myparserCorrecto testes/formal_list/t$testname.cl > $TMP2_OUT
+
+  if ! diff -qwB $TMP_OUT1 $TMP_OUT2 &>/dev/null; then
+    echo "Teste $testname falhou"
+  else
+    echo "Teste $testname passou"
+  fi
+done
+rm $TMP_OUT1
+rm $TMP_OUT2
