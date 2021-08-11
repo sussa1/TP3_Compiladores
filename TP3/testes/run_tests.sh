805d17ec6410613c701246891e673c25a85f4fd0
Vitor Rodarte <vitorricoy@gmail.com>
2021-08-11 15:34:16 -0300

    Correção do script

--- TP3/testes/run_tests.sh
+++ TP3/testes/run_tests.sh
@@ -2,8 +2,10 @@ echo "Testes Case List"
 
 for i in {1..4..1}; do
   testname=$(printf "%d" $i)
+  cd ..
   ./myparser case_list/t$testname.cl > test1.results
   ./myparserCorrecto case_list/t$testname.cl > test2.results
+  cd testes
 
   if ! diff -qwB $TMP_OUT1 $TMP_OUT2 &>/dev/null; then
     echo "Teste $testname falhou"
@@ -18,8 +20,10 @@ echo "Testes Class List"
 
 for i in {1..3..1}; do
   testname=$(printf "%d" $i)
+  cd ..
   ./myparser class_list/t$testname.cl > test1.results
   ./myparserCorrecto class_list/t$testname.cl > test2.results
+  cd testes
 
   if ! diff -qwB $TMP_OUT1 $TMP_OUT2 &>/dev/null; then
     echo "Teste $testname falhou"
@@ -34,8 +38,10 @@ echo "Testes Expression"
 
 for i in {1..2..1}; do
   testname=$(printf "%d" $i)
+  cd ..
   ./myparser expression/t$testname.cl > test1.results
   ./myparserCorrecto expression/t$testname.cl > test2.results
+  cd testes
 
   if ! diff -qwB $TMP_OUT1 $TMP_OUT2 &>/dev/null; then
     echo "Teste $testname falhou"
@@ -50,8 +56,10 @@ echo "Testes Expression Let"
 
 for i in {1..6..1}; do
   testname=$(printf "%d" $i)
+  cd ..
   ./myparser expression_let/t$testname.cl > test1.results
   ./myparserCorrecto expression_let/t$testname.cl > test2.results
+  cd testes
 
   if ! diff -qwB $TMP_OUT1 $TMP_OUT2 &>/dev/null; then
     echo "Teste $testname falhou"
@@ -66,8 +74,10 @@ echo "Testes Expression List Comma"
 
 for i in {1..4..1}; do
   testname=$(printf "%d" $i)
+  cd ..
   ./myparser expression_list_comma/t$testname.cl > test1.results
   ./myparserCorrecto expression_list_comma/t$testname.cl > test2.results
+  cd testes
 
   if ! diff -qwB $TMP_OUT1 $TMP_OUT2 &>/dev/null; then
     echo "Teste $testname falhou"
@@ -82,8 +92,10 @@ echo "Testes Expression List Semicolon"
 
 for i in {1..3..1}; do
   testname=$(printf "%d" $i)
+  cd ..
   ./myparser expression_list_semic/t$testname.cl > test1.results
   ./myparserCorrecto expression_list_semic/t$testname.cl > test2.results
+  cd testes
 
   if ! diff -qwB $TMP_OUT1 $TMP_OUT2 &>/dev/null; then
     echo "Teste $testname falhou"
@@ -98,8 +110,10 @@ echo "Testes Feature List"
 
 for i in {1..4..1}; do
   testname=$(printf "%d" $i)
+  cd ..
   ./myparser feature_list/t$testname.cl > test1.results
   ./myparserCorrecto feature_list/t$testname.cl > test2.results
+  cd testes
 
   if ! diff -qwB $TMP_OUT1 $TMP_OUT2 &>/dev/null; then
     echo "Teste $testname falhou"
@@ -114,8 +128,10 @@ echo "Testes Formal List"
 
 for i in {1..4..1}; do
   testname=$(printf "%d" $i)
+  cd ..
   ./myparser formal_list/t$testname.cl > test1.results
   ./myparserCorrecto formal_list/t$testname.cl > test2.results
+  cd testes
 
   if ! diff -qwB $TMP_OUT1 $TMP_OUT2 &>/dev/null; then
     echo "Teste $testname falhou"
