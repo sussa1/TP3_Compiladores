d4de1f106065e29a16b6738e244ee9fc43978e40
Vitor Rodarte <vitorricoy@gmail.com>
2021-08-21 18:46:19 -0300

    Corrige o problema

--- TP4/cgen.cc
+++ TP4/cgen.cc
@@ -1397,8 +1397,11 @@ void dispatch_class::code(ostream &s) {
   currentClass = oldClass;
   // Remove os parâmetros e novos atributos do estado e volta ao escopo antigo
   scopes.pop_back();
-  // Remove os parâmetros da pilha
-  unloadDataInStack(addedParametersIndexes, s);
+  // Essas funções dão pop no argumento dentro delas
+  if(address != "IO.out_string" || address != "IO.out_int") {
+    // Remove os parâmetros da pilha
+    unloadDataInStack(addedParametersIndexes, s);
+  }
   // Remove os atributos da pilha
   unloadDataInStack(addedAttributesIndexes, s);
 }
